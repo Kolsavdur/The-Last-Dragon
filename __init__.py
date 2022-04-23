70828650082dd84f1dd61cc28d838151dbee4af9
Kolsavdur <96321102+Kolsavdur@users.noreply.github.com>
2022-04-23 20:54:49 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from modloader.modclass import Mod, loadable_mod
 @loadable_mod
 class AWSWMod(Mod):
     name = "The Last Dragon"
-    version = "v1.05"
+    version = "v1.08"
     author = "Kolsavdur"
     dependencies = ["MagmaLink"]
 
